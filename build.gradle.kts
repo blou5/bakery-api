b4adc9b636b737f8e657200cccc95b04d9de7a00
Luka Buziu <lukabuziu22@gmail.com>
2026-05-04 14:30:47 +0200

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("org.postgresql:postgresql:42.7.11")
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly ("org.postgresql:postgresql")
     annotationProcessor ("org.projectlombok:lombok")
